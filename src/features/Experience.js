dc80e523211e71d53f08b16f0755936935fabff3
alexandremessang <alexandre.messang@gmail.com>
2020-10-31 16:37:09 +0100

    Add Experience component

---
+++ src/features/Experience.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { Box, Text, Heading, Carousel, Image } from 'grommet';
+
+export const Experience = () => {
+
+
+    return (
+        <Box>
+            <Box
+                height="small"
+            >
+                <Heading
+                    textAlign="center"
+                    color="#DB7F1F"
+                >
+                    {"Mes réalisations"}
+                </Heading>
+            </Box>  
+            <Box 
+                height="medium" 
+                width="large" 
+                overflow="hidden"
+                direction="row-responsive"
+            >
+                <Carousel 
+                    fill
+                    controls={false}
+                    play={5000}
+                >   
+                    <Box 
+                        as="a" 
+                        target="__blank" 
+                        fill={true}
+                        href="https://osev-mb.ext.carsat-mp.fr/" 
+                        style={{zIndex: 99, textDecoration: "none"}}
+                    >
+                        <Text as="p"
+                            size="large"
+                            textAlign="center"
+                            color="black"
+                        >
+                            {"Projet tutoré pour le compte de la Carsat MP"}
+                        </Text>
+                        <Image 
+                            fit="cover" 
+                            src="img/OSEV.png" 
+                        />
+                        
+                    </Box>
+                    <Box 
+                        as="a" 
+                        target="__blank" 
+                        style={{zIndex: 99, textDecoration: "none"}}
+                        width="full"
+                    >
+                        <Text as="p"
+                            size="large"
+                            textAlign="center"
+                            color="black"
+                        >
+                            {"Chambre d'hôtel sous l'eau en 3D"}
+                        </Text>
+                        <Image 
+                            fit="cover" 
+                            src="img/3D.png" 
+                        />
+                        
+                    </Box>
+                    <Box 
+                        as="a" 
+                        target="__blank" 
+                        style={{zIndex: 99, textDecoration: "none"}}
+                    >
+                        <Text as="p"
+                            size="large"
+                            textAlign="center"
+                            color="black"
+                        >
+                            {"Stage Laravel plateforme DPO pour le compte d'Altij"}
+                        </Text>
+                        <Image 
+                            fit="cover" 
+                            src="img/DPO.png" 
+                        />
+                        
+                    </Box>
+                </Carousel>
+            </Box>
+        </Box>
+            
+    )
+}
+
--- src/features/SecondSection.js
+++ src/features/SecondSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Box, Heading, Text, Image, Carousel } from 'grommet';
+import { Box, Heading, Text, Image } from 'grommet';
 import { AboutMe } from './AboutMe';
+import { Experience } from './Experience';
 
 
 export const SecondSection = () => {
@@ -21,86 +22,7 @@ export const SecondSection = () => {
            <Box 
                 basis="small" 
             />
-            <Box
-                    height="small"
-                >
-                    <Heading
-                        textAlign="center"
-                        color="#DB7F1F"
-                    >
-                        {"Mes réalisations"}
-                    </Heading>
-            </Box>  
-            <Box 
-                height="medium" 
-                width="large" 
-                overflow="hidden"
-                direction="row-responsive"
-            >
-                <Carousel 
-                    fill
-                    controls={false}
-                    play={5000}
-                >   
-                    <Box 
-                        as="a" 
-                        target="__blank" 
-                        fill={true}
-                        href="https://osev-mb.ext.carsat-mp.fr/" 
-                        style={{zIndex: 99, textDecoration: "none"}}
-                    >
-                        <Text as="p"
-                            size="large"
-                            textAlign="center"
-                            color="black"
-                        >
-                            {"Projet tutoré pour le compte de la Carsat MP"}
-                        </Text>
-                        <Image 
-                            fit="cover" 
-                            src="img/OSEV.png" 
-                        />
-                        
-                    </Box>
-                    <Box 
-                        as="a" 
-                        target="__blank" 
-                        style={{zIndex: 99, textDecoration: "none"}}
-                        width="full"
-                    >
-                        <Text as="p"
-                            size="large"
-                            textAlign="center"
-                            color="black"
-                        >
-                            {"Chambre d'hôtel sous l'eau en 3D"}
-                        </Text>
-                        <Image 
-                            fit="cover" 
-                            src="img/3D.png" 
-                        />
-                        
-                    </Box>
-                    <Box 
-                        as="a" 
-                        target="__blank" 
-                        style={{zIndex: 99, textDecoration: "none"}}
-                    >
-                        <Text as="p"
-                            size="large"
-                            textAlign="center"
-                            color="black"
-                        >
-                            {"Stage Laravel plateforme DPO pour le compte d'Altij"}
-                        </Text>
-                        <Image 
-                            fit="cover" 
-                            src="img/DPO.png" 
-                        />
-                        
-                    </Box>
-                </Carousel>
-            </Box>
+            <Experience />
             <Box 
                 basis="small" 
             />
